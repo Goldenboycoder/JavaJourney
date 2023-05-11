723240e3b95bea6f378d29ae1fd188ad2a896f04
goldenboycoder <goldenboycoder@gmail.com>
2023-05-11 19:32:36 +0300

    started equalstacks

---
+++ Exercises/DataStructures/EqualStacks.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class EqualStacks{
+
+
+    public static int equalStacks(List<Integer> h1, List<Integer> h2, List<Integer> h3) {
+        // Write your code here
+    
+    }
+    
+
+    public static void main(String[] args) {
+        //
+        List<Integer> h1 = new Stack<Integer>();
+
+        List<Integer> h2 = new Stack<Integer>({4,3,2});
+        List<Integer> h3 = new Stack<Integer>({1,1,4,1});
+
+        
+    }
+}
