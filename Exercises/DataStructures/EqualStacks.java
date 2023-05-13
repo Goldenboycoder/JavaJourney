543d58444631d6eb7ea7bc4bb612707380510926
goldenboycoder <goldenboycoder@gmail.com>
2023-05-13 13:20:09 +0300

    completed EqualStacks ex

--- Exercises/DataStructures/EqualStacks.java
+++ Exercises/DataStructures/EqualStacks.java
@@ -6,9 +6,36 @@ class EqualStacks{
     public static int equalStacks(List<Integer> h1, List<Integer> h2, List<Integer> h3) {
         // Write your code here
         // solution is to keep track on sum of list and compare then subtract on each iteration
-        int res = 0;
-        int h1s = h1.forEach(null);
-        return res;
+        int h1s = 0;
+        int h2s = 0;
+        int h3s = 0;
+        int min = -1;
+        for(int i : h1){
+            h1s+=i;
+        }
+        for(int i : h2){
+            h2s+=i;
+        }
+        for(int i : h3){
+            h3s+=i;
+        }
+
+        while(h1s != h2s ||  h2s != h3s){
+            min = Math.min(h1s, Math.min(h2s, h3s));
+           
+            if(h2s> min){
+                h2s-=h2.remove(0);
+            }
+            if(h1s> min){
+                h1s-=h1.remove(0);
+            }
+            if(h3s> min){
+                h3s-=h3.remove(0);
+            }
+            //System.out.println(String.format("H1: %1$s, H2: %2$s, H3: %3$s",  h1s,h2s,h3s));
+        }
+        
+        return min;
     }
     
 
@@ -23,7 +50,7 @@ class EqualStacks{
             h1.add(arr12);
         }
 
-        int[] arr2 = new int[]{4,3,2};
+        int[] arr2 = new int[]{0};//{4,3,2};
         for (int arr12 : arr2) {
             h2.add(arr12);
         }
