e805686c5d7a77a7f2bd8cf5211abb88018ddfa6
goldenboycoder <goldenboycoder@gmail.com>
2023-07-01 16:52:54 +0300

    Started the movie rental system  ex

---
+++ Exercises/OOP2/MovieRentalSystem.java
@@ -0,0 +1,32 @@
+package Exercises.OOP2;
+
+
+/*
+ * Will solve later
+ * https://leetcode.com/problems/design-movie-rental-system/solutions/1298866/java-hashmap-treeset-solution-clean-concise/
+ */
+
+import java.util.List;
+
+class MovieRentingSystem {
+
+    public MovieRentingSystem(int n, int[][] entries) {
+        
+    }
+    
+    public List<Integer> search(int movie) {
+        
+    }
+    
+    public void rent(int shop, int movie) {
+        
+    }
+    
+    public void drop(int shop, int movie) {
+        
+    }
+    
+    public List<List<Integer>> report() {
+        
+    }
+}
